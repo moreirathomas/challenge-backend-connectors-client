61dba304a74aa78b1bd5f001d3b4e61d47c3bdc0
moreirathomas <moreirathomas97@gmail.com>
2022-11-03 20:08:52 +0100

    feat: login

---
+++ src/auth/login.ts
@@ -0,0 +1,53 @@
+import fetch from "../node-fetch";
+
+interface Credentials {
+  login: string;
+  password: string;
+  client_id: string;
+  client_secret: string;
+}
+
+const API_URL = `${process.env["API_URL"]}/login`;
+
+export async function login(credentials: Credentials): Promise<string> {
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: stringifyAuthorization(credentials),
+    },
+    body: jsonifyBody(credentials),
+  });
+
+  if (response.status !== 200) {
+    console.error(await response.text());
+    throw new Error(`Response error: ${response.statusText}`);
+  }
+
+  const data = await response.json();
+  return parseLoginResponse(data).refresh_token;
+}
+
+const stringifyAuthorization = ({ client_id, client_secret }: Credentials) => {
+  return `Basic ${Buffer.from(`${client_id}:${client_secret}`).toString("base64")}`;
+};
+
+const jsonifyBody = ({ login, password }: Credentials) => {
+  return JSON.stringify({ user: login, password });
+};
+
+interface LoginResponse {
+  refresh_token: string;
+}
+
+const parseLoginResponse = (response: unknown): LoginResponse => {
+  if (typeof response !== "object" || response === null) {
+    throw new Error("Invalid response");
+  }
+  // TODO dedicated typeguards
+  const { refresh_token } = response as Record<string, unknown>;
+  if (typeof refresh_token !== "string") {
+    throw new Error("Invalid response");
+  }
+  return { refresh_token };
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log(process.env["API_URL"]);
+import { login } from "./auth/login";
+
+const getCredential = () => {
+  return {
+    login: process.env["LOGIN"] as string,
+    password: process.env["PASSWORD"] as string,
+    client_id: process.env["CLIENT_ID"] as string,
+    client_secret: process.env["CLIENT_SECRET"] as string,
+  };
+};
+
+async function main() {
+  const credentials = getCredential();
+  const rt = await login(credentials);
+  console.log(rt);
+}
+
+main().catch(console.error);
