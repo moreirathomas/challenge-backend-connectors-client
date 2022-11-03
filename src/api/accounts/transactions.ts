4674a97696a4ff68094aca95e0235b25e7189601
moreirathomas <moreirathomas97@gmail.com>
2022-11-03 20:46:35 +0100

    feat: transactions

---
+++ src/api/accounts/transactions.ts
@@ -0,0 +1,46 @@
+import fetch from "../../node-fetch";
+
+const apiURL = (accountNumber: string) => `${process.env["API_URL"]}/accounts/${accountNumber}/transactions`;
+
+export async function getAccountTransactions(accountNumber: string, accessToken: string) {
+  const response = await fetch(apiURL(accountNumber), {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (response.status !== 200) {
+    console.error(await response.text());
+    throw new Error(`Response error: ${response.statusText}`);
+  }
+
+  const data = await response.json();
+  return parseTransactionsResponse(data);
+}
+
+interface TransactionsResponse {
+  transactions: {
+    id: string;
+    label: number;
+    sign: "DBT" | "CDT";
+    amount: string; // TODO conversion to number
+    currency: "EUR";
+  }[];
+
+  link: unknown;
+}
+
+const parseTransactionsResponse = (response: unknown): TransactionsResponse => {
+  if (typeof response !== "object" || response === null) {
+    throw new Error("Invalid response");
+  }
+  const { transactions, link } = response as TransactionsResponse;
+  // TODO deep parsing
+  if (!Array.isArray(transactions)) {
+    throw new Error("Invalid response");
+  }
+  // TODO do not ignore link property
+  return { transactions, link };
+};
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { authenticate } from "./api/auth";
 import { getAllAccounts } from "./api/accounts";
+import { getAccountTransactions } from "./api/accounts/transactions";
 
 async function main() {
   const token = await authenticate();
 
   const accounts = await getAllAccounts(token);
   console.log("accounts", accounts);
+
+  for (const account of accounts.account) {
+    const transactions = await getAccountTransactions(account.acc_number, token);
+    console.log("transactions", transactions);
+  }
 }
 
 main().catch(console.error);
